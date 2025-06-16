d4c1e8eeda9fb9574ca8b00106c72758cfead5fd
RubenSantiagoUrrego <rurrego@innovapps.net>
2025-06-16 08:58:05 -0500

    Refactor loan request handling and update dependencies

---
+++ src/main/java/org/sci/serviciolibros/dto/SolicitudPrestamoDTO.java
@@ -0,0 +1,34 @@
+package org.sci.serviciolibros.dto;
+
+public class SolicitudPrestamoDTO {
+    private Long usuarioId;
+    private Long libroId;
+    private String fechaEntrega;
+
+    public SolicitudPrestamoDTO() {}
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Long getLibroId() {
+        return libroId;
+    }
+
+    public void setLibroId(Long libroId) {
+        this.libroId = libroId;
+    }
+
+    public String getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(String fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+}
+
